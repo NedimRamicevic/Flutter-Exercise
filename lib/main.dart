a0fa9b48d9f9e356ebdfc9e31292aec978499696
Nedim <mr.ramicevic@gmail.com>
2021-12-16 11:14:41 +0300

    toStateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
